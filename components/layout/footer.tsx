9d2560847f5d7ed5a0d835d1adb20768480ae4a6
tzuyi <a0988426059@gmail.com>
2023-11-20 17:31:37 +0800

    feat: pointer spring effect

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 function Footer() {
   return (
@@ -6,21 +9,37 @@ function Footer() {
       <h2 className="px-[10px] flex gap-1 items-center">
         看選前民調
         <Link href="/">
-          <svg
-            viewBox="0 0 131 18"
+          <motion.svg
+            viewBox="0 0 280 18"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className="w-[126px]"
+            className="w-[262px]"
+            initial="rest"
+            whileHover="hover"
+            animate="rest"
           >
             <g id="btn">
-              <path
+              <motion.path
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M107.571 0.247803L130.25 17.318H0.256836V15.161H123.923L106.483 2.0336L107.571 0.247803Z"
                 fill="#DD4D2A"
+                variants={{
+                  rest: {
+                    d: 'M107.571 0.247803L130.25 17.318H0.256836V15.161H123.923L106.483 2.0336L107.571 0.247803Z',
+                    transition: {
+                      type: 'spring',
+                    },
+                  },
+                  hover: {
+                    d: 'M243.921 0.87915L261.444 12.7501H0.5V11.2501H256.556L243.079 2.12103L243.921 0.87915Z',
+                    transition: {
+                      type: 'spring',
+                    },
+                  },
+                }}
               />
             </g>
-          </svg>
+          </motion.svg>
         </Link>
       </h2>
       <p className="text-xs px-[10px] mt-7 mb-[10px]">樹懶設計 shulian@gmail.com</p>
