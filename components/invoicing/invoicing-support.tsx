3883c0441c35172ec6d435a80819833ab5f6d100
tzuyi <a0988426059@gmail.com>
2023-11-23 18:49:29 +0800

    feat: invoicing-support component

--- components/invoicing/invoicing-support.tsx
+++ components/invoicing/invoicing-support.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import InvoicingFilter from '@/components/invoicing/Invoicing-filter';
+import InvoicingFilter from '@/components/invoicing/invoicing-filter';
+import InvoicingBar from '@/components/invoicing/invoicing-bar';
+import { INVOICING } from '@/configs/Invoicing';
 
 function InvoicingSupport() {
   const [filter, setFilter] = useState({
@@ -14,6 +16,21 @@ function InvoicingSupport() {
         filter={filter}
         setFilter={setFilter}
       />
+      <div className="mt-4">
+        <div className="flex">
+          <p className="text-xs text-black/50 w-14">縣市</p>
+          <p className="text-xs text-black/50">得票占比</p>
+        </div>
+        <ul className="mt-4">
+          <li className="flex">
+            <p className="text-xs w-14">基隆市</p>
+            <InvoicingBar
+              className="h-[14px] flex-1"
+              proportion={INVOICING.default}
+            />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
