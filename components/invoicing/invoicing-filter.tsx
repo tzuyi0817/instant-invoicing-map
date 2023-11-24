d34c7bd8cdbe2a38359c95d6225af88710056431
tzuyi <a0988426059@gmail.com>
2023-11-24 12:04:49 +0800

    feat: reset filter

--- components/invoicing/invoicing-filter.tsx
+++ components/invoicing/invoicing-filter.tsx
@@ -1,8 +1,14 @@
-import type { Dispatch, SetStateAction } from 'react';
+import type { Dispatch, SetStateAction, MouseEvent } from 'react';
 import Image, { type StaticImageData } from 'next/image';
 import ddpAvatar from '@/assets/images/avatar/ddp-avatar.png';
 import kmtAvatar from '@/assets/images/avatar/kmt-avatar.png';
 import pfpAvatar from '@/assets/images/avatar/pfp-avatar.png';
+
+interface Filter {
+  ddp: boolean;
+  kmt: boolean;
+  pfp: boolean;
+}
 
 interface Props {
   filter: {
@@ -10,13 +16,7 @@ interface Props {
     kmt: boolean;
     pfp: boolean;
   };
-  setFilter: Dispatch<
-    SetStateAction<{
-      ddp: boolean;
-      kmt: boolean;
-      pfp: boolean;
-    }>
-  >;
+  setFilter: Dispatch<SetStateAction<Filter>>;
 }
 
 interface FilterContentProps {
@@ -29,13 +29,21 @@ const filterStyle = 'h-6 rounded-[24px] transition-all border border-white flex 
 const filterText = 'text-white text-xs pl-5 flex-1 whitespace-nowrap';
 
 function InvoicingFilter({ filter, setFilter }: Props) {
+  function resetFilter(event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>, target: Filter) {
+    event.stopPropagation();
+    setFilter(target);
+  }
+
   return (
-    <div className="border border-black py-3 px-7 flex items-center gap-2 mr-3">
+    <div
+      className="border border-black py-3 px-7 flex items-center gap-2 mr-3"
+      onClick={event => resetFilter(event, { ddp: false, kmt: false, pfp: false })}
+    >
       <p>最支持</p>
       <div className="flex">
         <div
           className={`bg-deep-green ${filterStyle} ${filter.ddp ? 'w-[104px] duration-300' : 'w-6 duration-0'} z-[2]`}
-          onClick={() => setFilter({ ddp: true, kmt: false, pfp: false })}
+          onClick={event => resetFilter(event, { ddp: true, kmt: false, pfp: false })}
         >
           <FilterContent
             filter={filter.ddp}
@@ -47,7 +55,7 @@ function InvoicingFilter({ filter, setFilter }: Props) {
           className={`bg-deep-blue ${filterStyle} ${
             filter.kmt ? 'w-[104px] duration-300' : 'w-6 duration-0'
           } -translate-x-3 z-[1]`}
-          onClick={() => setFilter({ ddp: false, kmt: true, pfp: false })}
+          onClick={event => resetFilter(event, { ddp: false, kmt: true, pfp: false })}
         >
           <FilterContent
             filter={filter.kmt}
@@ -59,7 +67,7 @@ function InvoicingFilter({ filter, setFilter }: Props) {
           className={`bg-deep-orange ${filterStyle} ${
             filter.pfp ? 'w-[104px] duration-300' : 'w-6 duration-0'
           } -translate-x-6`}
-          onClick={() => setFilter({ ddp: false, kmt: false, pfp: true })}
+          onClick={event => resetFilter(event, { ddp: false, kmt: false, pfp: true })}
         >
           <FilterContent
             filter={filter.pfp}
