26fac215430ea8b9335c44bdc09f8356f541dea5
tzuyi <a0988426059@gmail.com>
2023-11-26 19:34:16 +0800

    feat: presidential palace color mask

--- components/home/home-vote-ratio.tsx
+++ components/home/home-vote-ratio.tsx
@@ -2,17 +2,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 import PaperPresidential from '@/assets/images/layout/paper-presidential.png';
 import PresidentialPalace from '@/assets/images/layout/presidential-palace.png';
+import { INVOICING } from '@/configs/Invoicing';
 
 const PseudoElement =
   'before:content-[""] before:transition-transform before:h-4 before:border-l-[0.5px] before:absolute before:border-black before:-top-4';
 
 function HomeVoteRatio() {
+  const { kmt, ddp, pfp } = INVOICING.default;
+
   return (
     <div className="px-4 border-b-[3px] border-black">
       <div className="pt-7 border-x border-black flex flex-col items-center">
         <div className="w-[260px] h-[130px] bg-black rounded-t-full"></div>
         <div className="relative h-[130px] w-full">
-          <div className="absolute w-full -top-[100px]">
+          <div className="absolute w-full bottom-[52px]">
             <Image
               src={PaperPresidential}
               alt="paper presidential"
@@ -26,10 +29,37 @@ function HomeVoteRatio() {
               className="w-full object-cover"
               priority
             />
-            <ul className="w-full absolute bottom-0 flex text-xs px-1">
-              <li className={`w-1/3 ${PseudoElement}`}>蔡英文 0%</li>
-              <li className={`w-1/3 ${PseudoElement}`}>韓國瑜 0%</li>
-              <li className={`w-1/3 ${PseudoElement}`}>宋楚瑜 0%</li>
+            <div
+              className="absolute bottom-0 left-3 w-[calc(100%-24px)] h-full opacity-50"
+              style={{
+                maskImage: `url(${PresidentialPalace.src})`,
+                WebkitMaskImage: `url(${PresidentialPalace.src})`,
+                maskSize: 'cover',
+                WebkitMaskSize: 'cover',
+                background: `linear-gradient(to right,#25a55c 0%,#25a55c ${ddp}%,#4a8fe7 ${ddp}%,#4a8fe7 ${
+                  ddp + kmt
+                }%,#f88545 ${ddp + kmt}%,#f88545 100%)`,
+              }}
+            ></div>
+            <ul className="w-full absolute bottom-0 left-0 flex text-xs px-4">
+              <li
+                className={`${PseudoElement}`}
+                style={{ width: `${ddp}%` }}
+              >
+                蔡英文 {ddp}%
+              </li>
+              <li
+                className={`${PseudoElement}`}
+                style={{ width: `${kmt}%` }}
+              >
+                韓國瑜 {kmt}%
+              </li>
+              <li
+                className={`whitespace-nowrap ${PseudoElement}`}
+                style={{ width: `${pfp}%` }}
+              >
+                <p className="-translate-x-[300%]">宋楚瑜 {pfp}%</p>
+              </li>
             </ul>
           </div>
         </div>
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,50 +1,66 @@
 'use client';
 
+import { useMemo } from 'react';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function Footer() {
+  const pathname = usePathname();
+  const isHomePage = useMemo(() => pathname === '/', [pathname]);
+
   return (
-    <footer className="w-full px-6 py-[22px] bg-black text-white overflow-hidden">
-      <h2 className="px-[10px] flex gap-1 items-center">
-        <p className="whitespace-nowrap">看選前民調</p>
-        <Link href="/poll">
-          <motion.svg
-            viewBox="0 0 280 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-[262px]"
-            initial="rest"
-            whileHover="hover"
-            animate="rest"
+    <AnimatePresence mode="wait">
+      <footer className="w-full px-6 py-[22px] bg-black text-white overflow-hidden flex flex-col gap-7">
+        {isHomePage && (
+          <motion.h2
+            className="px-[10px] flex gap-1 items-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
           >
-            <g id="btn">
-              <motion.path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                fill="#DD4D2A"
-                variants={{
-                  rest: {
-                    d: 'M107.571 0.247803L130.25 17.318H0.256836V15.161H123.923L106.483 2.0336L107.571 0.247803Z',
-                    transition: {
-                      type: 'spring',
-                    },
-                  },
-                  hover: {
-                    d: 'M243.921 0.87915L261.444 12.7501H0.5V11.2501H256.556L243.079 2.12103L243.921 0.87915Z',
-                    transition: {
-                      type: 'spring',
-                    },
-                  },
-                }}
-              />
-            </g>
-          </motion.svg>
-        </Link>
-      </h2>
-      <p className="text-xs px-[10px] mt-7 mb-[10px]">樹懶設計 shulian@gmail.com</p>
-      <p className="text-xs px-[10px]">© 2023 樹懶設計 shulian 版權所有</p>
-    </footer>
+            <p className="whitespace-nowrap">看選前民調</p>
+            <Link href="/poll">
+              <motion.svg
+                viewBox="0 0 280 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-[262px]"
+                initial="rest"
+                whileHover="hover"
+                animate="rest"
+              >
+                <g id="btn">
+                  <motion.path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    fill="#DD4D2A"
+                    variants={{
+                      rest: {
+                        d: 'M107.571 0.247803L130.25 17.318H0.256836V15.161H123.923L106.483 2.0336L107.571 0.247803Z',
+                        transition: {
+                          type: 'spring',
+                        },
+                      },
+                      hover: {
+                        d: 'M243.921 0.87915L261.444 12.7501H0.5V11.2501H256.556L243.079 2.12103L243.921 0.87915Z',
+                        transition: {
+                          type: 'spring',
+                        },
+                      },
+                    }}
+                  />
+                </g>
+              </motion.svg>
+            </Link>
+          </motion.h2>
+        )}
+        <div>
+          <p className="text-xs px-[10px] mb-[10px]">樹懶設計 shulian@gmail.com</p>
+          <p className="text-xs px-[10px]">© 2023 樹懶設計 shulian 版權所有</p>
+        </div>
+      </footer>
+    </AnimatePresence>
   );
 }
 
