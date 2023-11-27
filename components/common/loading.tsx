125176a68435c409ad032c13ec7bdcbe4cdd3880
tzuyi <a0988426059@gmail.com>
2023-11-27 17:56:21 +0800

    feat: loading min-width: 1024px rwd

--- components/common/loading.tsx
+++ components/common/loading.tsx
@@ -9,41 +9,41 @@ import { sleep } from '@/utils/common';
 
 const messages = [
   {
-    className: 'absolute -top-[30%] -right-[75%] w-[163px] md:w-[266px]',
+    className: 'absolute -top-[30%] -right-[75%] w-[163px] md:w-[266px] lg:w-[315px]',
     translateX: '30%',
     translateY: '-50%',
     message: '逮灣發大財',
   },
   {
-    className: 'absolute top-0 -left-[65%] w-[122px] md:w-[198px]',
+    className: 'absolute top-0 -left-[65%] w-[122px] md:w-[198px] lg:w-[234px]',
     translateX: '-30%',
     translateY: '-50%',
     message: '凍蒜!凍蒜!',
     isWhite: true,
   },
   {
-    className: 'absolute top-[30%] -right-[75%] w-[122px] md:w-[198px]',
+    className: 'absolute top-[30%] -right-[75%] w-[122px] md:w-[198px] lg:w-[234px]',
     translateX: '30%',
     translateY: '30%',
     message: '2020台灣要贏',
     isWhite: true,
   },
   {
-    className: 'absolute top-[55%] -left-[55%] w-[100px] md:w-[120px]',
+    className: 'absolute top-[55%] -left-[55%] w-[100px] md:w-[120px] lg:w-[142px]',
     translateX: '-30%',
     translateY: '30%',
     message: '點亮台灣',
     rotateClass: 'rotate-180',
   },
   {
-    className: 'absolute top-[95%] -left-[85%] w-[164px] md:w-[266px]',
+    className: 'absolute top-[95%] -left-[85%] w-[164px] md:w-[266px] lg:w-[315px]',
     translateX: '-30%',
     translateY: '50%',
     message: 'Taiwan No.1',
     isWhite: true,
   },
   {
-    className: 'absolute top-[85%] -right-[30%] w-[122px] md:w-[198px]',
+    className: 'absolute top-[85%] -right-[30%] w-[122px] md:w-[198px] lg:w-[234px]',
     translateX: '30%',
     translateY: '50%',
     message: '結束藍綠對峙',
@@ -87,17 +87,17 @@ function Loading() {
       <motion.div
         className="fixed top-0 left-0 w-screen h-screen bg-white flex justify-center items-center z-20"
         onAnimationComplete={onAnimationComplete}
-        // animate={{
-        //   opacity: 0,
-        //   transition: { delay: 1.2 },
-        // }}
+        animate={{
+          opacity: 0,
+          transition: { delay: 1.2 },
+        }}
       >
         <div className="relative">
           <motion.div animate={controls}>
             <Image
               src={VoteStamp}
               alt="vote stamp"
-              className="w-[153px] md:w-[250px]"
+              className="w-[153px] md:w-[250px] lg:w-[296px]"
             />
           </motion.div>
           {messages.map(({ message, ...item }) => {
