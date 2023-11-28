c1bfad1f61e61bc9d8549943504e0b012483d89a
tzuyi <a0988426059@gmail.com>
2023-11-28 23:46:01 +0800

    refactor: useResize hook

--- hooks/useResize.tsx
+++ hooks/useResize.tsx
@@ -2,9 +2,19 @@ import { useEffect, useRef } from 'react';
 import { debounce } from '@/utils/common';
 
 function useResize(callback: () => void) {
-  const callbackRef = useRef(debounce(callback));
+  const previousWidth = useRef(0);
+  const callbackRef = useRef(
+    debounce(() => {
+      const { innerWidth } = window;
+
+      if (innerWidth === previousWidth.current) return;
+      previousWidth.current = innerWidth;
+      callback();
+    }),
+  );
 
   useEffect(() => {
+    previousWidth.current = window.innerWidth;
     window.addEventListener('resize', callbackRef.current);
 
     return () => window.removeEventListener('resize', callbackRef.current);
